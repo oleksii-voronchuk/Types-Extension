eb3931a039ad9d832ee7dc2a25d7a461e7ad9c14
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-02-24 22:05:07 +0200

    Vector3 extensions added

---
+++ Assets/Tools/OLVOR/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Tools.OLVOR.Scripts.Extensions
+{
+    /// <summary>
+    /// Set of extensions for the Vector3 type.
+    /// </summary>
+    public static class Vector3Extensions
+    {
+        /// <summary>
+        /// Set x value of an existing Vector3.
+        /// </summary>
+        public static void ChangeX(this ref Vector3 vector, float value)
+        {
+            vector.Set(value, vector.y, vector.z);
+        }
+
+        /// <summary>
+        /// Set y value of an existing Vector3.
+        /// </summary>
+        public static void ChangeY(this ref Vector3 vector, float value)
+        {
+            vector.Set(vector.x, value, vector.z);
+        }
+
+        /// <summary>
+        /// Set z value of an existing Vector3.
+        /// </summary>
+        public static void ChangeZ(this ref Vector3 vector, float value)
+        {
+            vector.Set(vector.x, vector.y, value);
+        }
+
+        /// <summary>
+        /// Set random values of x, y, z within [minInclusive..maxInclusive] (range is inclusive). Then returns the vector.
+        /// </summary>
+        public static Vector3 Random(this ref Vector3 vector, float minInclusive, float maxInclusive)
+        {
+            vector.Set(UnityEngine.Random.Range(minInclusive, maxInclusive), UnityEngine.Random.Range(minInclusive, maxInclusive), UnityEngine.Random.Range(minInclusive, maxInclusive));
+            return vector;
+        }
+    }
+}
