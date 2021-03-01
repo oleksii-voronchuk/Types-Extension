1a0631175f0945fabbf7164360712e130c9d1658
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-03-01 23:19:44 +0200

    Int and Float extensions added

---
+++ Assets/Tools/OLVOR/Scripts/Extensions/FloatExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assets.Tools.OLVOR.Scripts.Extensions
+{
+    public static class FloatExtensions
+    {
+        /// <summary>
+        /// Changes the value of a variable to random within [minInclusive..maxInclusive] (range is inclusive).
+        /// </summary>
+        public static float Random(this ref float value, float minInclusive, float maxInclusive)
+        {
+            var newRandom = UnityEngine.Random.Range(minInclusive, maxInclusive);
+            if (Math.Abs(newRandom - value) < 0.001f)
+            {
+                value = value + 1 >= maxInclusive ? minInclusive : ++value;
+            }
+            else
+            {
+                value = newRandom;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Assets/Tools/OLVOR/Scripts/Extensions/IntExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Assets.Tools.OLVOR.Scripts.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Changes the value of a variable to random within [minInclusive..maxInclusive] (range is inclusive).
+        /// </summary>
+        public static int Random(this ref int value, int minInclusive, int maxInclusive)
+        {
+            var newRandom = UnityEngine.Random.Range(minInclusive, maxInclusive);
+            if (newRandom == value)
+            {
+                value = value == maxInclusive ? minInclusive : ++value;
+            }
+            else
+            {
+                value = newRandom;
+            }
+
+            return value;
+        }
+    }
+}
