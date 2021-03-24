63f8c487cae0d4735a9f6298fe93e556db67c882
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-03-24 20:29:50 +0200

    Added random extensions.

---
+++ Assets/Tools/OLVOR/Scripts/Extensions/RandomExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Tools.OLVOR.Scripts.Extensions
+{
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Return the array of unique random integers within [minInclusive..maxInclusive] (range is inclusive).
+        /// </summary>
+        public static int[] RandomIntArray(int count, int minInclusive, int maxInclusive)
+        {
+            var array = new int[count];
+            var availableInts = new List<int>();
+
+            for (var i = minInclusive; i <= maxInclusive; i++)
+            {
+                availableInts.Add(i);
+            }
+
+            if (count > availableInts.Count) return array;
+            for (var i = 0; i < count; i++)
+            {
+                var value = Random.Range(0, availableInts.Count);
+                array[i] = value;
+                availableInts.Remove(value);
+            }
+
+            return array;
+        }
+    }
+}
